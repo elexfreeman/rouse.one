1942bb72b31697ce3e35cc99580eeabcc619374a
elex <elextraza@gmial.com>
2018-04-01 13:25:27 +0400

    Маршруты в корзине

--- react-template/src/CartComponent/CartApp.jsx
+++ react-template/src/CartComponent/CartApp.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router-dom'
+import { Route, IndexRoute } from 'react-router'
 
 import CartComponent from "./CartComponent";
 import DeliveryComponent from "./DeliveryComponent";
--- react-template/src/cart.jsx
+++ react-template/src/cart.jsx
@@ -2,38 +2,49 @@ import React from 'react';
 import {render} from 'react-dom';
 // redux
 import {Provider} from 'react-redux';
-import { ConnectedRouter } from 'react-router-redux'
+import {syncHistoryWithStore} from 'react-router-redux';
+import { Router, Route, browserHistory } from 'react-router';
 import createHistory from 'history/createBrowserHistory';
-import { syncHistoryWithStore } from 'react-router-redux';
 
 import store from './redux/store';
 
 
-// const history = syncHistoryWithStore(createBrowserHistory(), store);
-const history = createHistory();
+ // const history = syncHistoryWithStore(createBrowserHistory(), store);
+//const history = createHistory();
+//const history = syncHistoryWithStore(browserHistory, store);
+const history = syncHistoryWithStore(createHistory(), store);
 import CartApp from './CartComponent/CartApp';
 
 
 import './styles/main.scss';
 import CartButton from "./cart/CartButton";
 import {onGetCart} from './redux/actions/cart';
-let cart_container =  document.getElementById('cart_container');
+import DeliveryComponent from "./CartComponent/DeliveryComponent";
+import CartComponent from "./CartComponent/CartComponent";
+
+let cart_container = document.getElementById('cart_container');
 
 // Корзина
 /*загружаем ее*/
 store.dispatch(onGetCart());
 
-render(<Provider store={store}>
-    <ConnectedRouter history={history}>
-        <div>
-            <CartApp />
-        </div>
-    </ConnectedRouter>
-</Provider>, cart_container);
 
+
+render(<Provider store={store}>
+    <div>
+        <Router history={history}>
+            <div>
+                <Route path="/" component={CartComponent} />
+                <Route path="/delivery" component={DeliveryComponent}/>
+            </div>
+
+
+        </Router>
+    </div>
+</Provider>, cart_container);
 
 
 // кнопка корзины
 render(<Provider store={store}>
-    <CartButton />
+    <CartButton/>
 </Provider>, document.getElementById('cart_button'));
